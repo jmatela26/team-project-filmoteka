a7de2c2c4d63293e15d8966c57700572cb107cf0
jmatela26 <jmatela26@gmail.com>
2024-02-25 03:01:07 +0800

    Still fixing the footer modal team

--- src/js/templates/markupTeamModal.js
+++ src/js/templates/markupTeamModal.js
@@ -1,17 +1,15 @@
-const image = "../../images/team/avatar.png"
+const image = '../../images/favicon.png';
 
 const teamModalMarkup = `
 
 <div class="footer-modal__window">
+    <a href=""  target="_blank" class="social-list__item">
+        <img src="${image}" alt="team" class="team__list team__img">
+        <div class="team__box">
+            <h3 class="team-logo__name">Our team </h3>
+        </div>
+    </a>
     <ul class="team">
-        <li class="team__list">
-            <a href=""  target="_blank" class="social-list__item">
-                <img src="${image}" alt="team" class="team__list team__img">
-                <div class="team__box">
-                    <h3 class="team-logo__name">Our team </h3>
-                </div>
-            </a>
-        </li>
         <li class="team__list">
             <a href="" target="_blank" class="social-list__item">
                 <img src="${image}" alt="Josiah Matela" class="team__img">
@@ -23,7 +21,7 @@ const teamModalMarkup = `
         </li>
         <li class="team__list">
             <a href="" target="_blank" class="social-list__item">
-                <img src="${image}" alt="Les John Paul" class="team__img ">
+                <img src="" alt="Les John Paul" class="team__img ">
                 <div class="team__box">
                     <h3 class="team__name">Les John Paul</h3>
                     <p class="team__work">Scrum Master</p>
@@ -32,7 +30,7 @@ const teamModalMarkup = `
         </li>
         <li class="team__list">
             <a href="" target="_blank" class="social-list__item">
-                <img src="${image}" alt="Ruby Mateo" class="team__img">
+                <img src="" alt="Ruby Mateo" class="team__img">
                 <div class="team__box">
                     <h3 class="team__name">Ruby Mateo</h3>
                     <p class="team__work">Developer</p>
@@ -41,7 +39,7 @@ const teamModalMarkup = `
         </li>
         <li class="team__list">
             <a href="" target="_blank" class="social-list__item">
-                <img src="${image}" alt="" class="team__img">
+                <img src="" alt="" class="team__img">
                 <div class="team__box">
                     <h3 class="team__name">Michelle Jean Sudaria</h3>
                     <p class="team__work">Developer</p>
@@ -50,7 +48,7 @@ const teamModalMarkup = `
         </li>
         <li class="team__list">
             <a href="h" target="_blank" class="social-list__item">
-                <img src="${image}" alt="" class="team__img">
+                <img src="" alt="" class="team__img">
                 <div class="team__box">
                     <h3 class="team__name">Rene Fernandez</h3>
                     <p class="team__work">Developer</p>
@@ -59,7 +57,7 @@ const teamModalMarkup = `
         </li>
         <li class="team__list">
             <a href="h" target="_blank" class="social-list__item">
-                <img src="${image}" alt="" class="team__img">
+                <img src="" alt="" class="team__img">
                 <div class="team__box">
                     <h3 class="team__name">Cindy Ruth Orllena</h3>
                     <p class="team__work">Developer</p>
